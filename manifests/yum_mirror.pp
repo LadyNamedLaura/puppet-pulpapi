8dc68e0b8901f08590116a39d6b47baae8c32d4a
Simon Peeters <simon@inuits.eu>
2016-11-16 12:34:44 +0100

    cleanup mirror repo id

--- manifests/yum_mirror.pp
+++ manifests/yum_mirror.pp
@@ -4,8 +4,8 @@ define pulpapi::yum_mirror (
   $upstream,
   $relative_url = $title,
 ) {
-  $id = regsubst($title, '\W', '_', 'G')
-  pulpapi::yum_repo{"mirror_${id}":
+  $id = regsubst(regsubst($title, '\W', '_', 'G'), '_$','')
+  pulpapi::yum_repo{"${id}":
     relative_url     => "pub/${relative_url}",
     upstream         => $upstream,
     sync_schedule    => $::pulpapi::mirror_schedule,
