ed6bbbed230c2ef0024bc0c0d09306039633974a
Simon Peeters <simon@inuits.eu>
2016-12-09 14:06:00 +0100

    make yum_repo ensurable

--- manifests/yum_repo.pp
+++ manifests/yum_repo.pp
@@ -2,6 +2,7 @@
 #
 define pulpapi::yum_repo (
   $id                = $title,
+  $ensure            = present,
   $relative_url      = "$title",
   $upstream          = '/.empty',
   $sync_schedule     = undef,
@@ -11,56 +12,58 @@ define pulpapi::yum_repo (
   $allow_upload_from = [],
 ) {
   pulp_repo{$id :
-    ensure => 'present',
+    ensure => $ensure,
     notes  => {
       managed_by   => 'puppet',
       '_repo-type' => 'rpm-repo',
     }
   }
 
-  $importer_config = {
-    feed             => $upstream ? {
-      false   => undef,
-      default => regsubst($upstream,'^/',$::pulpapi::httpurl),
-    },
-    retain_old_count => $retain_old_count,
-    remove_missing   => $remove_missing,
-  }
-
-  pulp_importer{$id :
-    ensure  => present,
-    type    => 'yum_importer',
-    config  => delete_undef_values($importer_config),
-    require => Pulp_repo[$id],
-  }
-
-  if $sync_schedule {
-    pulp_sync_schedule{$id :
-      ensure  => present,
-      sched   => $sync_schedule,
-      require => Pulp_importer[$id],
+  if $ensure == present {
+    $importer_config = {
+      feed             => $upstream ? {
+        false   => undef,
+        default => regsubst($upstream,'^/',$::pulpapi::httpurl),
+      },
+      retain_old_count => $retain_old_count,
+      remove_missing   => $remove_missing,
     }
-  }
 
-  if ! empty($allow_upload_from) {
-    $uploadperms = suffix($allow_upload_from, "::${id}")
-    ::pulpapi::permission::repo_upload{$uploadperms:}
-  }
+    pulp_importer{$id :
+      ensure  => present,
+      type    => 'yum_importer',
+      config  => delete_undef_values($importer_config),
+      require => Pulp_repo[$id],
+    }
 
-  pulp_distributor{"${id}__yum_distributor":
-    ensure       => present,
-    id           => 'yum_distributor',
-    type         => 'yum_distributor',
-    repo         => $id,
-    auto_publish => ($upstream and $sync_schedule),
-    config       => {
-      checksum_type   => "sha256",
-      http            => true,
-      https           => false,
-      relative_url    => $relative_url,
-      generate_sqlite => true,
-#      repoview => $repoview,
-    },
-    require      => Pulp_repo[$id],
+    if $sync_schedule {
+      pulp_sync_schedule{$id :
+        ensure  => present,
+        sched   => $sync_schedule,
+        require => Pulp_importer[$id],
+      }
+    }
+
+    if ! empty($allow_upload_from) {
+      $uploadperms = suffix($allow_upload_from, "::${id}")
+      ::pulpapi::permission::repo_upload{$uploadperms:}
+    }
+
+    pulp_distributor{"${id}__yum_distributor":
+      ensure       => present,
+      id           => 'yum_distributor',
+      type         => 'yum_distributor',
+      repo         => $id,
+      auto_publish => ($upstream and $sync_schedule),
+      config       => {
+        checksum_type   => "sha256",
+        http            => true,
+        https           => false,
+        relative_url    => $relative_url,
+        generate_sqlite => true,
+  #      repoview => $repoview,
+      },
+      require      => Pulp_repo[$id],
+    }
   }
 }
