e1bde8e6512b9b3fab927345415b0283afe97ec3
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-07-25 22:58:11 +0200

    Fix config file & remove custom fact
    This fixes the template for the config, adds tests and removes the
    custom fact by using stdlib's puppet_vardir fact
    
    Closes GH-1

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,12 +2,22 @@ require 'spec_helper'
 
 describe 'pulpapi' do
   let :facts do
-    {:pulp_apiconfig_path => '/tmp/pulpapi.json'}
+    {
+      :fqdn          => 'foo.example.com',
+      :puppet_vardir => '/var/lib/puppet',
+    }
   end
 
   context 'default parameters' do
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_pulpapi__yum_repo('__empty__') }
+    it do
+      is_expected.to contain_file('/var/lib/puppet/pulpapi.json')
+        .with_content(%r{"apiurl": "https://foo\.example\.com/pulp/api/v2/"})
+        .with_content(%r{"httpurl": "http://foo\.example\.com/pulp/repos/"})
+        .with_content(%r{"apiuser": "admin"})
+        .with_content(%r{"apipass": "admin"})
+    end
   end
 
   context 'with overrides' do
